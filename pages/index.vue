9b81a192987b1b92df8e4cb54e592bc50ed2bf7a
ScriptKev <script.kev@gmail.com>
2020-12-10 09:45:32 -0500

    adding index maintenance

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,87 @@
 <template>
-  <div>
-    <Header />
-    <Main />
-    <!-- <Services /> -->
-    <Pray />
+  <div class="bgMantenimiento">
+    <div class="mantenimiento__container px-4 pb-2">
+      <div class="mantenimiento__grid">
+        <section class="align-self-center">
+          <img src="~/static/img/logo.png" width="100" />
+        </section>
+
+        <section class="d-flex flex-column align-center main__title align-self-center text-center">
+          <IconHomeMaintenance />
+          <h3>Estamos Trabajando</h3>
+          <span>Para servirte Mas y mejor</span>
+        </section>
+
+        <section class="d-flex flex-column align-center align-self-center">
+          <span>siguenos en:</span>
+          <div class="d-flex justify-center align-center">
+            <v-icon size="35" class="mx-2">mdi-instagram</v-icon>
+            <v-icon size="35" class="mx-2">mdi-facebook</v-icon>
+            <v-icon size="35" class="mx-2">mdi-youtube</v-icon>
+            <span>LCP-CARACAS</span>
+          </div>
+          <p class="text-center mt-4">
+            Edf. Santa Fe, nivel Mezzanina. Calle Tito Salas. Bellas Artes - Caracas.
+            <br/>
+            Telefonos: 0212-578.59.69 / 0424-149.88.00
+          </p>
+        </section>
+      </div>
+    </div>
   </div>
 </template>
 
+<style scoped>
+  .bgMantenimiento {
+    width: 100%;
+    height: 100%;
+    background-image: url('../static/img/Construccion-min.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+
+  .mantenimiento__grid {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 20vh 50vh 30vh;
+    justify-items: center;
+  }
+
+  .main__title h3 {
+    font-size: 50px;
+    text-transform: uppercase;
+
+  }
+
+  .main__title span {
+    font-size: 30px;
+    text-transform: uppercase;
+  }
+
+  @media screen and (min-width: 320px) and (max-width: 500px) {
+    .main__title h3 {
+      font-size: 30px;
+    }
+
+    .main__title span {
+      font-size: 20px;
+      text-transform: uppercase;
+    }
+
+    p {
+      margin-bottom: 0;
+    }
+  }
+</style>
+
 <script>
-import Header from '~/components/PageSections/Home/Header'
-import Main from '~/components/PageSections/Home/Main'
-import Pray from '~/components/PageSections/Home/Pray'
-import Services from '~/components/PageSections/Home/Services'
-import Worship from '~/components/PageSections/Home/Worship'
+import IconHomeMaintenance from '~/components/icons-components/IconHomeMaintenance'
 
 export default {
-  name: 'Home',
-  components: { Header, Main, Pray, Services, Worship },
+  name: 'index',
+  components: { IconHomeMaintenance }
 }
 </script>
